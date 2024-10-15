765449f730a8fbdcbf0023c279abf6202297a0d8
AdityaDeCW <67094373+CommieP@users.noreply.github.com>
2024-10-15 14:07:11 +0530

    Corsfix (maybe)

--- src/packages/ViewerCanvas/index.jsx
+++ src/packages/ViewerCanvas/index.jsx
@@ -1,23 +1,27 @@
-// ViewerPage.js
-import React from 'react';
+import React, { Suspense } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { useLoader } from '@react-three/fiber';
 
+const Model = ({ modelUrl }) => {
+    const gltf = useLoader(GLTFLoader, modelUrl);
+    return <primitive object={gltf.scene} scale={0.5} />;
+};
+
 const ViewerCanvas = () => {
     const location = useLocation();
     const { modelUrl } = location.state; // Get the model URL from state
-
-    const gltf = useLoader(GLTFLoader, modelUrl);
 
     return (
         <div className="viewer-page">
             <Canvas>
                 <ambientLight intensity={0.5} />
                 <directionalLight position={[10, 10, 10]} />
-                <primitive object={gltf.scene} scale={0.5} />
+                <Suspense> {/* Suspense with fallback */}
+                    <Model modelUrl={modelUrl} />
+                </Suspense>
                 <OrbitControls />
             </Canvas>
         </div>
